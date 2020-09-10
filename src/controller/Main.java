dbd068e591609bf1bd99728f5f3992fedaa14d1e
chaitanyab23 <chaitanyabhagat23@gmail.com>
2020-09-10 22:54:15 +0530

    chaitanya

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,24 @@
+package controller;
 
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// object to fetch all skills
+		SkillDAO skilldao = new SkillDAO();
+
+		// arraylist to add the skill
+		List<Skill> skills = skilldao.listAllSkills();
+
+		// printing all the skill
+		System.out.println("List of all Skill");
+		System.out.println();
+		for (int i = 0; i < skills.size(); i++) {
+			System.out.println((i + 1) + ". " + skills.get(i).getSkillName());
+		}
+
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,35 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO {
+	public List<Skill> listAllSkills() throws Exception {
+
+	// Arraylist to add skill to the list
+	List<Skill> skills = new ArrayList<Skill>();
+	
+	//connection to the database
+	Connection connection= ConnectionManager.getConnection();
+	
+	//statement to fetch all skills from database
+	Statement statement= connection.createStatement();
+	
+	//store the fetch results
+	ResultSet resultSet= statement.executeQuery("SELECT * FROM skill");
+	
+	//adding the skill to the list
+	while(resultSet.next()) {
+		Skill skill=new Skill(resultSet.getLong("id"),resultSet.getString("name"));
+		skills.add(skill);
+	}
+	return skills;
+	}
+
+}
